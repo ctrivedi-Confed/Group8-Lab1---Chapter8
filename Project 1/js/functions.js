285e89c4594847d130781783e63498f5fae57dc8
Nilkumar Patel <=>
2021-01-18 10:29:40 -0500

    Group 8 Lab 1 Chapter 8 - step 5

--- Project 1/js/functions.js
+++ Project 1/js/functions.js
@@ -6,5 +6,11 @@ function calculateTotal(price, quantity) {
 }
 
 function outputCartRow(file, title, quantity, price, total) {
-
+    document.write('<tr>' +
+                        '<td><img src="' + file + '"></td>' +
+                        '<td>' + title + '</td>' +
+                        '<td>' + quantity + '</td>' +
+                        '<td>' + price + '</td>' +
+                        '<td>' + total + '</td>' +
+                    '</tr>')
 }
