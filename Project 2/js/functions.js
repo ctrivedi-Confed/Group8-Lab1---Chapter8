a550434da436b2628c78512a9d084df59976ae90
Chintan Trivedi <chintantrivedi247@gmail.com>
2021-01-27 16:07:05 -0500

    Group8-Lab1-project2: Step3: declaring outputcountrybox function

--- Project 2/js/functions.js
+++ Project 2/js/functions.js
@@ -1,1 +1,14 @@
 /* add in your functions here */
+
+function outputCountryBox(name, continent, cities, photos){
+    document.write('<div class="item">');
+    document.write('<h2>' + name +'</h2><p>' + continent +'</p>');
+    //call functions to display cities and images
+    outputCities();
+    outputPhotos();
+     //declare functions for displaying cities and photos
+    function outputCities(){
+    }
+    function outputPhotos(){
+    }
+}
