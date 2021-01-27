55a28204b64a8a4d0bfaedbc6d6a60d08566a47b
Nilkumar Patel <=>
2021-01-27 16:40:26 -0500

    Group8-Lab1-Project2 Step 4 (outputcities and outputphotos)

--- Project 2/js/functions.js
+++ Project 2/js/functions.js
@@ -8,7 +8,18 @@ function outputCountryBox(name, continent, cities, photos){
     outputPhotos();
      //declare functions for displaying cities and photos
     function outputCities(){
+        document.write('<div class="inner-box"><h3>Cities</h3><ul>');    
+        for(let i=0; i<cities.length; i++){
+            document.write('<li>'+ cities[i] +'</li>')
+        }
+        document.write('</ul></div>');
     }
     function outputPhotos(){
+        document.write('<div class="inner-box"><h3>Popular Photos</h3>');
+
+        for(let i=0; i<photos.length; i++){
+            document.write(' <img src="images/' + photos[i] +'.jpg" class="photo">')
+        }
     }
+    document.write('</div><button>Visit</button></div>');
 }
