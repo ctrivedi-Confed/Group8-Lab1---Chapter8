8fdc8be1033102706724ab7e1d8caf31a30b52b1
crahol <raholchaitali20@gmail.com>
2021-01-27 15:38:01 -0500

    Group8 Lab1 Project 2 - Step 2 (Data.js)

--- Project 2/js/data.js
+++ Project 2/js/data.js
@@ -1,1 +1,7 @@
 /* define your array of object literals here */
+
+countries = [['Canada', 'North America',['Calgary', 'Montreal', 'Toronto'],['canada1', 'canada2', 'canada3']] 
+    , ['United States', 'North America', ['Boston', 'Chicago', 'New York', 'Seattle', 'Washington'],['us1', 'us2']]
+    , ['Italy','Europe', ['Florence', 'Milan', 'Naples', 'Rome'],['italy1', 'italy2', 'italy3', 'italy4', 'italy5', 'italy6']]
+    , ['Spain','Europe', ['Almeria', 'Barcelona', 'Madrid'],['spain1', 'spain2']]
+    ]
